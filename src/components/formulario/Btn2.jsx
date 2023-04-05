fd9fed105feaa75d1c195455f61a723b64de584c
NGCnathalia <nthaliagarcia@gmail.com>
2023-04-05 00:46:28 -0500

    Changes in form contact

--- src/components/formulario/Btn2.jsx
+++ src/components/formulario/Btn2.jsx
@@ -1,12 +1,19 @@
 import React from 'react'
 import '../formulario/formContact.css'
+import Swal from 'sweetalert2'
 
 function Btn2() {
+
+    const handleClick = () => {
+        Swal.fire('Mensaje enviado', '', 'success');        
+    }
+
     return (
         <div className='divBtn2'>
-            <button type="submit" className='divBtn'>
+            <button type="submit" className='divBtn' onClick={handleClick}>
                 Enviar Mensaje
             </button>
+            
         </div>
     )
 }
--- src/components/formulario/FormContact.jsx
+++ src/components/formulario/FormContact.jsx
@@ -3,22 +3,37 @@ import '../formulario/formContact.css'
 import { Form, Formik } from 'formik'
 import { InputsField } from './InputsField'
 import Btn2 from './Btn2'
+import  { useRef } from 'react'
+import emailjs from 'emailjs-com'
+
+
 export const FormContact = () => {
+
+    const form = useRef(); 
+    const sendEmail = (e) => {
+      e.preventDefault();
+  
+      emailjs.sendForm('service_i0itial', 'template_xivgi9f', form.current, 'pC6960epC_S05YpDK')
+  
+      e.target.reset()
+    }
+
     return (
         <div>
             <Formik initialValues={{
                     name: '',
                     email: '',
-                    phone: '',
+                    Asunto: '',
+                    Mensaje: '',
                 }}  onSubmit={(values, { resetForm }) => {
                     console.log(values)
                     resetForm()}} >
                 {formik => (
-                    <Form>
-                        <InputsField label="Nombre" name="name" type="text" placeholder="Ingrese su nombre completo." />
-                        <InputsField label="Correo electrónico" name="email" type="email" placeholder="Ingrese su correo electrónico." />
-                        <InputsField label="Asunto" name="Asunto" type="text" placeholder="Ingrese el asunto." />
-                        <InputsField label="Mensaje" name="Mensaje" type="text" placeholder="Ingrese su mensaje." />
+                    <Form ref={form} onSubmit={sendEmail} >
+                        <InputsField label="Nombre" name="name" type="text" placeholder="Ingrese su nombre completo." required />
+                        <InputsField label="Correo electrónico" name="email" type="email" placeholder="Ingrese su correo electrónico." required/>
+                        <InputsField label="Asunto" name="Asunto" type="text" placeholder="Ingrese el asunto." required />
+                        <InputsField label="Mensaje" name="Mensaje" type="text" placeholder="Ingrese su mensaje." required />
                         <Btn2/>
                     </Form>
                 )}
